36a4244a937079186d03a8558e3690f1af9f7616
Dongzhe (Denzel) Zheng <dz5992@princeton.edu>
2026-09-04 16:04:35 -0400

    Align Comparator statement imports with the coordinate model

--- Challenge.lean
+++ Challenge.lean
@@ -1,4 +1,9 @@
-import Mathlib
+import Mathlib.Combinatorics.SimpleGraph.Finite
+import Mathlib.Data.Nat.Find
+import Mathlib.Data.Real.Basic
+import Mathlib.LinearAlgebra.Dimension.Constructions
+import Mathlib.LinearAlgebra.FiniteDimensional.Lemmas
+import Mathlib.Tactic.Ring
 
 /-!
 # Three-dimensional body--pin rigidity: statement surface
